7a978e37f298938ab13573f26bfc8d0088a1cf6c
Domenic Cacace <DMC1542@rit.edu>
2021-01-02 15:06:18 -0500

    initial commit

---
+++ src/Start.java
@@ -0,0 +1,16 @@
+/**
+ * The entry point for the program. Runs the simulation by sending telemetry packets.
+ *
+ * @author Domenic Cacace
+ *
+ * Language: Java 15
+ * File: Start.java
+ *
+ * Date Created: 1/2/2021
+ * Last Modified: 1/2/2021
+ */
+
+public class Start
+{
+
+}
